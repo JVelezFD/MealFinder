cd8bb0808d2ad1006b500d2f4743cab608f3e13d
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-06-15 18:51:58 -0400

    working youtube iframe and updated api key

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
-// let recipe=document.getElementById("recipe");
-// //google custom search api
-// let googleapi='AIzaSyAokFh6lKdcKUYeUx-39ZeqJAHVwyocZmk';
-// let apiCall='https://www.googleapis.com/customsearch/v1?key=AIzaSyAokFh6lKdcKUYeUx-39ZeqJAHVwyocZmk&cx=0376c30bf308d4095&q=breakfastrecipe';
-//  fetch(apiCall)
-//         .then(function(response) {
-//           return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//             console.log(data.items[1].htmlTitle);
-//             var a= data.items;
-//             var b = a.length;
+let recipe=document.getElementById("recipe");
+//google custom search api
+let googleapi='AIzaSyAokFh6lKdcKUYeUx-39ZeqJAHVwyocZmk';
+let apiCall='https://www.googleapis.com/customsearch/v1?key=AIzaSyAokFh6lKdcKUYeUx-39ZeqJAHVwyocZmk&cx=0376c30bf308d4095&q=breakfastrecipe';
+ fetch(apiCall)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data.items[1].htmlTitle);
+            var a= data.items;
+            var b = a.length;
         
-//   for(i=0;i<=b-1;i++){
-// var recipe1=document.createElement('p');
-// recipe.appendChild(recipe1);
-//  recipe1.innerHTML=data.items[i].htmlTitle;
+  for(i=0;i<=b-1;i++){
+var recipe1=document.createElement('p');
+recipe.appendChild(recipe1);
+ recipe1.innerHTML=data.items[i].htmlTitle;
 
-//   }
-//  youtubeAPI;
-//  });
+  }
+ youtubeAPI;
+ });
 
 
 
 var meal = "fish-tacos"; //sample meal name
 
-//get actual value of "meal" from google search/recipe??
+//get actual value of "meal" from google search/recipe?
 displayVideo(meal);
 
 function displayVideo(meal) {
@@ -34,8 +34,7 @@ var mealName = meal;
 
 var recipeVidTitleEl = $(".recipeVidTitle");
 var recipeVidFrameEl = $(".recipeVidFrame");
-//old key: AIzaSyD-YLZ0by6WplSCkblIufQSI-2tE_W5Vpw
-//prev key: AIzaSyAbaqrxrs3TdOu7sGbZyN_ZPWOnXRH0aKs
+
 var ytApiKey = "AIzaSyD4MIilTmWgkRGpkukDcnqegu0wJP1Q-Qk";
 var searchLink = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + mealName + '&key=' + ytApiKey;
 
