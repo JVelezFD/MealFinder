02357e0bb5b17e2dbc4dda61f32628bd90f9eaed
oliviaguerra731@gmail.com <102640672+nightbird99@users.noreply.github.com>
2022-06-17 19:45:54 -0400

    added video description to display page

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,11 +165,11 @@ next1.addEventListener("click", function() {
   //}
  //youtubeAPI;
  //});
-var meal = "fish-tacos"; //sample meal name 
+//var meal = "fish-tacos"; //sample meal name 
 
 
 //get actual value of "meal" from google search/recipe?
-displayVideo(meal);
+//displayVideo(meal);
 
 function displayVideo(meal) {
 //youtube api
@@ -177,8 +177,10 @@ var mealName = meal;
 
 var recipeVidTitleEl = $(".recipeVidTitle");
 var recipeVidFrameEl = $(".recipeVidFrame");
+var videoDescriptEl = $('.videoDescript');
 
-var ytApiKey = "AIzaSyD4MIilTmWgkRGpkukDcnqegu0wJP1Q-Qk";
+//var ytApiKey = "AIzaSyD4MIilTmWgkRGpkukDcnqegu0wJP1Q-Qk";
+var ytApiKey = 'AIzaSyDDJMMUoCy1yD_I63tWvovosXeOiq0frZU';
 var searchLink = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + mealName + '&key=' + ytApiKey;
 
 fetch(searchLink)
@@ -187,14 +189,16 @@ fetch(searchLink)
   })
   .then(function (data) {
     // console.log('Raw data from search results');
-    // console.log("data: ");
-    // console.log(data);
+    console.log("data: ");
+    console.log(data);
     // console.log('Individual search result items:');
     // console.log("data.item: ");
     // console.log(data.items);
     recipeVidTitleEl.text(data.items[0].snippet.title);
     var videoLink = "https://www.youtube.com/embed/" + data.items[0].id.videoId;
     recipeVidFrameEl.attr("src",videoLink);
+    videoDescriptEl.text(data.items[0].snippet.description);
+    console.log(data.items[0].snippet.description);
   });
 };
   
